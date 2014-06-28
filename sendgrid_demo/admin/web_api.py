6ad19f1091f061b80f09807af2fa1ef73a9a197b
Nick Simko <simko33f@gmail.com>
2014-06-28 16:20:16 -0600

    Adding Django project files

---
+++ sendgrid_demo/admin/web_api.py
@@ -0,0 +1,84 @@
+"""
+Program that handles the Web API calls
+"""
+import argparse
+import sendgrid
+import urllib
+
+#Authentication Credentials
+USERNAME = 'stimko68'
+PASSWORD = '6gpk2PXv5GyD'
+
+
+class WebAPI(object):
+
+    def __init__(self, user, api_key):
+        self.user = user
+        self.api_key = api_key
+        self.base_url = 'https://api.sendgrid.com/api/'
+
+    def get_stats(self, days=None, start_date=None, end_date=None, aggregate=None, category=None):
+        option = 'stats.get.json'
+        auth = "?api_user={}&api_key={}".format(self.user, self.api_key)
+        options = {
+            'days': days,
+            'start_date': start_date,
+            'end_date': end_date,
+            'aggregate': aggregate,
+            'category': category,
+        }
+        extras = ''
+        for key, value in options.items():
+            if value is not None:
+                extras += "&{}={}".format(key, value)
+        response = urllib.urlopen(self.base_url+option+auth+extras)
+        return response.read()
+
+    def send_email(self, recipients, sender, subject, message_body):
+        sg = sendgrid.SendGridClient(self.user, self.api_key)
+        message = sendgrid.Mail(to=recipients, from_email=sender, subject=subject, text=message_body)
+        return sg.send(message)
+
+    def view_blocks(self, date=None, days=None, start_date=None, end_date=None, limit=None, offset=None):
+        option = 'blocks.get.json'
+        auth = "?api_user={}&api_key={}".format(self.user, self.api_key)
+        options = {
+            'date': date,
+            'days': days,
+            'start_date': start_date,
+            'end_date': end_date,
+            'limit': limit,
+            'offset': offset,
+        }
+        extras = ''
+        for key, value in options.items():
+            if value is not None:
+                extras += "&{}={}".format(key, value)
+        response = urllib.urlopen(self.base_url+option+auth+extras)
+        return response.read()
+
+    def view_bounces(self, date=None, days=None, start_date=None, end_date=None, limit=None,
+                     offset=None, bounce_type=None, email=None):
+        option = 'bounces.get.json'
+        auth = "?api_user={}&api_key={}".format(self.user, self.api_key)
+        options = {
+            'date': date,
+            'days': days,
+            'start_date': start_date,
+            'end_date': end_date,
+            'limit': limit,
+            'offset': offset,
+            'type': bounce_type,
+            'email': email,
+        }
+        extras = ''
+        for key, value in options.items():
+            if value is not None:
+                extras += "&{}={}".format(key, value)
+        response = urllib.urlopen(self.base_url+option+auth+extras)
+        return response.read()
+
+
+if __name__ == "__main__":
+    api = WebAPI(USERNAME, PASSWORD)
+    print api.send_email('nick.simko@mrworf.com', 'simko33f@gmail.com', 'Testing', 'Test from Python script in demo app')
---
+++ sendgrid_demo/sendgrid_demo/urls.py
@@ -0,0 +1,14 @@
+from admin import views
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'sendgrid_demo.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index),
+    url(r'^dashboard/', views.dashboard),
+)
